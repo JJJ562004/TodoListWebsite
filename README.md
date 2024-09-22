532abcccd65e8640c999ccac7003a0c1d494ac03
JJJ562004 <darknessrisesgamer999@gmail.com>
2024-09-22 20:11:31 +0700

    Updated, added login.html, added user class as new table  with along change in main.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 A simple todolist website. You can create a individual group of your tasks.
 Should a task is completed, you can click on it to mark it as done.
+
+TODO_NEXT:
+Figure out the id of user work in html files, each user has its own pages.
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String, Boolean
 from datetime import datetime
 
@@ -37,12 +37,21 @@ class TodoFrom(FlaskForm):
 
 # CREATE TABLE
 
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150))
+    password = db.Column(db.String(50))
+    user_list = db.relationship('List', backref='author')
+
+
 class List(db.Model):
     __tablename__ = 'list'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
     date = db.Column(db.DateTime)
     list_item = db.relationship('ToDo', backref='author')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class ToDo(db.Model):
@@ -58,24 +67,54 @@ with app.app_context():
 
 
 @app.route('/')
+def start():
+    return render_template('login.html')
+
+
+@app.route('/home')
 def home():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=username, password=password).first()
+        if user:
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+        flash('Invalid credentials, please try again.')
+
+        return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return render_template('login.html')
+
+
 @app.route('/all_lists', methods=['GET'])
-def show_all():
-    result_list = List.query.all()
-    result_todo = ToDo.query.all()
+def show_all(user_id):
+    result_list = List.query.get_or_404(user_id)
+    result_todo = ToDo.query.filter_by(list_id=result_list.id).all()
     return render_template('all.html', all_list=result_list, all_todo=result_todo)
 
 
 @app.route('/list_create', methods=['POST', 'GET'])
 def create_list():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('login'))
     form_list = ListForm()
     if form_list.validate_on_submit():
         new_list = List(
             name=form_list.name.data,
             date=datetime.now(),
+            user_id=user_id
         )
         db.session.add(new_list)
         db.session.commit()
@@ -106,6 +145,7 @@ def toggle_check(todo_id):
         todo_item.is_checked = not todo_item.is_checked
         db.session.commit()
     return redirect(url_for('show_all'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
